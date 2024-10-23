226adfa3af6fed6f3d7c3ad41efb04a421a14837
ZurielNava <zuriel.nava.hernandez@gmail.com>
2024-10-23 11:55:11 -0600

    23/10/202

---
+++ stock_manager/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+class Server{
+    constructor(){
+        this.app = express();
+        this.port=3000;
+        this.app.use(express.json());
+    }
+
+
+    start(){
+        this.app.listen(this.port, () => {
+            console.log('server listening on port ' + this.port);
+        });
+    }
+}
+
+module.exports = {Server};
